5225d367b2456c9d75d893b2c2bca667a2a02cd8
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2021-11-21 20:01:25 -0300

    add modal on exclusions

--- public/js/global.js
+++ public/js/global.js
@@ -6,15 +6,22 @@ $(".cancelar-logout").on("click", () => {
   $("#modal-logout").modal("hide");
 })
 
-
-$(".btn-excluir-cliente").on("click", () => {
+function excluirCliente(id) {
+  document.querySelector("#excluir-cliente").setAttribute("action",`/clientes/excluir/${id}?_method=DELETE`)
   $("#modal-excluir-cliente").modal("show");
-})
+}
 
 $(".cancelar-excluir-cliente").on("click", () => {
   $("#modal-excluir-cliente").modal("hide");
 })
 
-let clientes
-let cliente = ejs.render('<%=clientes %>', {clientes : clientes})
-console.log(clientes)
+
+
+function excluirHistorico(id) {
+  document.querySelector("#excluir-historico").setAttribute("action",`/clientes/historico/excluir/${id}?_method=DELETE`)
+  $("#modal-excluir-historico").modal("show");
+}
+
+$(".cancelar-excluir-historico").on("click", () => {
+  $("#modal-excluir-historico").modal("hide");
+})
