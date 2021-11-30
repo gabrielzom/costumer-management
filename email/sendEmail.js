f134a16958b2e3a1ae19844a9e34de2c910c211b
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2021-11-30 01:13:26 -0300

    add email on send

--- email/sendEmail.js
+++ email/sendEmail.js
@@ -1,28 +1,46 @@
 const nodemailer = require("nodemailer")
+const mg = require('nodemailer-mailgun-transport')
 require("dotenv").config()
 
 module.exports = (req, res) => {
 
-  const user = process.env.USER_EMAIL
-  const pass = process.env.PASS_EMAIL
+  let auth = {
+    auth : {
+      api_key : "f5321727ba69fbfbe9b15f821e1e23a3",
+      domain : "sandbox1c8e049178cf41a787cc6193ccee55fd.mailgun.org"
+    }
+  }
 
   const transporter = nodemailer.createTransport({
 
     host : process.env.SMTP_HOST,
     port : 587,
-    secure: false,
     auth : {
-      user,
-      pass
-    }  
+      user : process.env.USER_EMAIL,
+      pass : process.env.PASS_EMAIL
+    }
+    
   })
 
   transporter.sendMail({
-      from : process.env.USER_EMAIL,
-      to : ["gabriel.marombinha@gmail.com", process.env.USER_EMAIL, "gabrielnascimento_17@hotmail.com"],
+      from : "gabrielnascimento_17@hotmail.com",
+      to : "gabrielnascimento_17@hotmail.com",
       replyTo : process.env.USER_EMAIL,
-      subject: "TESTE EMAIL NODEMAILER",
-      text : "FODA DEMAIS !!!"
+      subject : "no reply",
+      html :  
+        `
+        <!DOCTYPE HTML>
+        <html>
+        <head>
+        </head>
+        <body>
+          <a href="https://costumer-management.herokuapp.com/usuario/login" target="_blank">
+            <button>Confirmar e-mail e fazer Login</button>
+          </a>
+        </body>
+        </html>
+        
+        `
     })
 
     .then((info) => {
